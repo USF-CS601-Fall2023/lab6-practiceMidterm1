670975ccbf90d49918d387f23fe7b054fe07f0ad
Olga Karpenko <okarpenko@usfca.edu>
2023-10-06 11:46:54 -0700

    Added the starter code for lab 6.

---
+++ src/main/java/immutability/Hotel.java
@@ -0,0 +1,48 @@
+package immutability;
+
+// FILL IN CODE: make the class immutable
+public class Hotel {
+    private String hotelName;
+    private String hotelId;
+    private Address address;
+
+    /**
+     * Constructor to set values of the variables
+     * @param hotelName
+     * @param hotelId
+     * @param address
+     */
+    public Hotel(String hotelName, String hotelId, Address address) {
+        this.hotelName = hotelName;
+        this.hotelId = hotelId;
+        this.address = address;
+    }
+
+    /**
+     * @return hotelId.
+     */
+    public String getHotelId() {
+        return hotelId;
+    }
+
+    public Address getAddress() { return address; }
+
+    /**
+     * Set the new address
+     * @param newAddress
+     */
+    public void setAddress(Address newAddress) {
+        this.address = address;
+    }
+
+    /**
+     * @return a string representing the hotel
+     */
+    @Override
+    public String toString() {
+        return  "hotelName=" + hotelName + System.lineSeparator() +
+                "hotelId=" + hotelId + System.lineSeparator() +
+                 address.toString();
+    }
+
+}
